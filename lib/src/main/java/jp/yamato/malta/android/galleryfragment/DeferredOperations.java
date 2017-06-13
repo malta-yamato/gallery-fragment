67a1f45fa845ab4293f08195f51fcb20ccbd4090
Yamato Malta <malta.yamato@gmail.com>
2017-06-13 21:11:24 +0900

    minor change

--- lib/src/main/java/jp/yamato/malta/android/galleryfragment/DeferredOperations.java
+++ lib/src/main/java/jp/yamato/malta/android/galleryfragment/DeferredOperations.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright (C) 2017 MALTA-YAMATO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.yamato.malta.android.galleryfragment;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * Created by Takeru on 2016/03/06.
- * DeferredOperations
- */
 public class DeferredOperations {
 
     private Queue<Runnable> mQueue;
--- lib/src/main/java/jp/yamato/malta/android/galleryfragment/GalleryDialogFragment.java
+++ lib/src/main/java/jp/yamato/malta/android/galleryfragment/GalleryDialogFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 MALTA-YAMATO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.yamato.malta.android.galleryfragment;
 
 import android.annotation.TargetApi;
@@ -13,10 +29,6 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-/**
- * Created by malta on 2017/05/02.
- * GalleryFragment
- */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
 public class GalleryDialogFragment extends DialogFragment {
     @SuppressWarnings("unused")
