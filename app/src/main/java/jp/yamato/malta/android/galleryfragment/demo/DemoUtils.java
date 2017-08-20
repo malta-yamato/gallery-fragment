67649d9fb14885a27c0cae22c9f6dc07e43e2568
Yamato Malta <malta.yamato@gmail.com>
2017-08-20 14:43:53 +0900

    Create GalleryFragmentDemo_B.

--- app/src/main/java/jp/yamato/malta/android/galleryfragment/demo/DemoUtils.java
+++ app/src/main/java/jp/yamato/malta/android/galleryfragment/demo/DemoUtils.java
@@ -20,14 +20,29 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class DemoUtils {
+    @SuppressWarnings("unused")
+    private static final String TAG = "DemoUtils";
+
+    private static final int IMAGE_BUF_SIZE = 8 * 1048576;
 
     public static void startActivity(Context context, Uri uri) {
         if (uri == null) {
@@ -73,6 +88,220 @@ public class DemoUtils {
         cursor.close();
 
         return list;
+    }
+
+    // Since AsyncTask used by the ImageAdapter runs in series, there is no need to synchronize.
+    public static void writeBitmapToCache(Context context, Uri uri, Bitmap bitmap) {
+        if (uri == null || bitmap == null) {
+            return;
+        }
+
+        // write cache
+        File cacheFile = getCacheFile(context, uri);
+        BufferedOutputStream stream = null;
+        try {
+            stream = new BufferedOutputStream(new FileOutputStream(cacheFile));
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // Since AsyncTask used by the ImageAdapter runs in series, there is no need to synchronize.
+    public static Bitmap readBitmapFromCache(Context context, Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+
+        // read cache if exist
+        File cacheFile = getCacheFile(context, uri);
+        if (cacheFile.exists()) {
+            BufferedInputStream stream = null;
+            try {
+                stream = new BufferedInputStream(new FileInputStream(cacheFile));
+                return BitmapFactory.decodeStream(stream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @NonNull
+    private static File getCacheFile(Context context, @NonNull Uri uri) {
+        // cache name
+        String path = uri.getPath();
+        String cacheName =
+                path.replace('/', '_').replace('\\', '_').replace(':', '_').replace(',', '_')
+                        .replace(';', '_') + ".png";
+
+        // cache dir
+        File dir = new File(context.getExternalCacheDir(), "thumbnails");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        return new File(dir, cacheName);
+    }
+
+    public static Bitmap createBitmapByDecodedStream(ContentResolver resolver, Uri uri,
+            int destWidth, int destHeight, int mode) {
+        Bitmap bitmap = null;
+        BufferedInputStream stream = null;
+        try {
+            // create buffered stream
+            InputStream in = resolver.openInputStream(uri);
+            if (in == null) {
+                return null;
+            }
+            stream = new BufferedInputStream(in, IMAGE_BUF_SIZE);
+
+            // check size
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(stream, null, options);
+            stream.close();
+            int orgWidth = options.outWidth;
+            int orgHeight = options.outHeight;
+            Log.v(TAG, "Original Image Size: " + orgWidth + " x " + orgHeight);
+
+            // resolve inSampleSize
+            int inSampleSize;
+            if (mode == 0) {
+//                widthRatio = (int) ((float) orgWidth / (float) destWidth);
+//                heightRatio = (int) ((float) orgHeight / (float) destHeight);
+//                inSampleSize = Math.min(widthRatio, heightRatio);
+
+                inSampleSize =
+                        getLeastRegulatedSampleSize(orgWidth, orgHeight, destWidth, destHeight);
+
+                Log.v(TAG, "inSampleSize: " + inSampleSize);
+            } else {
+                inSampleSize =
+                        getMostRegulatedSampleSize(orgWidth, orgHeight, destWidth, destHeight);
+                Log.v(TAG, "inSampleSize: " + inSampleSize);
+            }
+            // create buffered stream
+            in = resolver.openInputStream(uri);
+            if (in == null) {
+                return null;
+            }
+            stream = new BufferedInputStream(in, IMAGE_BUF_SIZE);
+
+            // load bitmap
+            options = new BitmapFactory.Options();
+            options.inSampleSize = inSampleSize;
+            options.inPreferredConfig = Bitmap.Config.RGB_565;
+            Bitmap decodedBitmap = BitmapFactory.decodeStream(stream, null, options);
+            stream.close();
+            int decodedWidth = decodedBitmap.getWidth();
+            int decodedHeight = decodedBitmap.getHeight();
+            Log.v(TAG, "Decoded Image Size: " + decodedWidth + " x " + decodedHeight);
+
+            // clipping
+            int clippingX = (decodedWidth - destWidth) / 2;
+            int clippingY = (decodedHeight - destHeight) / 2;
+            if (clippingX >= 0 && clippingY >= 0) {
+                Log.v(TAG, "!!!---Case A---!!!");
+                bitmap = Bitmap.createBitmap(decodedBitmap, clippingX, clippingY, destWidth,
+                        destHeight);
+                decodedBitmap.recycle();
+            } else if (clippingX < 0 && clippingY < 0) {
+                Log.v(TAG, "!!!---Case B---!!!");
+                int offsetX = -1 * clippingX;
+                int offsetY = -1 * clippingY;
+                Bitmap baseBitmap =
+                        Bitmap.createBitmap(destWidth, destHeight, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(baseBitmap);
+                canvas.drawBitmap(decodedBitmap, offsetX, offsetY, null);
+                bitmap = baseBitmap;
+                decodedBitmap.recycle();
+            } else if (clippingX < 0) {
+                Log.v(TAG, "!!!---Case C---!!!");
+                int offsetX = -1 * clippingX;
+                Bitmap baseBitmap =
+                        Bitmap.createBitmap(destWidth, destHeight, Bitmap.Config.ARGB_8888);
+                Bitmap clippedBitmap =
+                        Bitmap.createBitmap(decodedBitmap, 0, clippingY, decodedWidth, destHeight);
+                Canvas canvas = new Canvas(baseBitmap);
+                canvas.drawBitmap(clippedBitmap, offsetX, 0, null);
+                bitmap = baseBitmap;
+                decodedBitmap.recycle();
+                clippedBitmap.recycle();
+            } else {
+                Log.v(TAG, "!!!---Case D---!!!");
+                int offsetY = -1 * clippingY;
+                Bitmap baseBitmap =
+                        Bitmap.createBitmap(destWidth, destHeight, Bitmap.Config.ARGB_8888);
+                Bitmap clippedBitmap =
+                        Bitmap.createBitmap(decodedBitmap, clippingX, 0, destWidth, decodedHeight);
+                Canvas canvas = new Canvas(baseBitmap);
+                canvas.drawBitmap(clippedBitmap, 0, offsetY, null);
+                bitmap = baseBitmap;
+                decodedBitmap.recycle();
+                clippedBitmap.recycle();
+            }
+
+            int lastWidth = bitmap.getWidth();
+            int lastHeight = bitmap.getHeight();
+            Log.v(TAG, "Last Image Size: " + lastWidth + " x " + lastHeight);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return bitmap;
+    }
+
+    private static int getMostRegulatedSampleSize(int width, int height, int tgtWidth,
+            int tgtHeight) {
+        int sampleSize;
+        for (sampleSize = 1; sampleSize <= 16; sampleSize++) {
+            if (width % sampleSize == 0 && height % sampleSize == 0) {
+                if (width / sampleSize <= tgtWidth && height / sampleSize <= tgtHeight) {
+                    break;
+                }
+            }
+        }
+        return sampleSize;
+    }
+
+    private static int getLeastRegulatedSampleSize(int width, int height, int tgtWidth,
+            int tgtHeight) {
+        int sampleSize;
+        for (sampleSize = 16; sampleSize >= 1; sampleSize--) {
+            if (width % sampleSize == 0 && height % sampleSize == 0) {
+                if (width / sampleSize >= tgtWidth && height / sampleSize >= tgtHeight) {
+                    break;
+                }
+            }
+        }
+        return sampleSize;
     }
 
 }
---
+++ app/src/main/java/jp/yamato/malta/android/galleryfragment/demo/GalleryFragmentDemo_B.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (C) 2017 MALTA-YAMATO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.yamato.malta.android.galleryfragment.demo;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.yamato.malta.android.galleryfragment.FormatterPickable;
+import jp.yamato.malta.android.galleryfragment.GalleryFragment;
+import jp.yamato.malta.android.galleryfragment.GalleryFragmentParams;
+import jp.yamato.malta.android.galleryfragment.ImageAdapter;
+
+public class GalleryFragmentDemo_B extends AppCompatActivity
+        implements ImageAdapter.LoadTask.BitmapLoader, ImageAdapter.OnItemClickListener,
+        FormatterPickable {
+    @SuppressWarnings("unused")
+    private static final String TAG = "GalleryFragmentDemo_A";
+
+    private static final int IMAGE_COUNT = 1000;
+
+    private GalleryFragment mFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gallery_fragment_demo);
+
+        // fragment
+        if (savedInstanceState == null) {
+            // create fragment
+            mFragment = createFragment();
+
+            // commit
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, mFragment)
+                    .commit();
+
+            // set images
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || checkPermission()) {
+                setImages(mFragment);
+            }
+        } else {
+            mFragment =
+                    (GalleryFragment) getSupportFragmentManager().findFragmentById(R.id.container);
+        }
+
+    }
+
+    private GalleryFragment createFragment() {
+        GalleryFragment fragment =
+                GalleryFragment.newInstance(R.layout.gallery_image_container_256);
+        fragment.setLayout(GalleryFragmentParams.LINEAR_LAYOUT_VERTICAL, 0);
+        return fragment;
+    }
+
+    private void setImages(GalleryFragment fragment) {
+        fragment.setAdapterData(DemoUtils.getSampleUriFromExternalContent(this, IMAGE_COUNT));
+    }
+
+    @Override
+    public Bitmap loadBitmap(ContentResolver resolver, Uri uri) {
+        int requestSquareSize = getResources().getDimensionPixelSize(R.dimen.size_256dp);
+        Bitmap bitmap = DemoUtils.readBitmapFromCache(this, uri);
+        if (bitmap == null) {
+            bitmap = DemoUtils.createBitmapByDecodedStream(resolver, uri, requestSquareSize,
+                    requestSquareSize, 0);
+            DemoUtils.writeBitmapToCache(this, uri, bitmap);
+        }
+
+        return bitmap;
+    }
+
+    @Override
+    public void onItemClick(View view, ImageAdapter adapter, int position) {
+        DemoUtils.startActivity(this, adapter.getAdapterDataItem(position));
+    }
+
+    //
+    // Formatter
+    //
+    @Override
+    public Map<String, ImageAdapter.Formatter> pickFormatter() {
+        Map<String, ImageAdapter.Formatter> map = new HashMap<>();
+        map.put(ImageAdapter.FILE_NAME, new ImageAdapter.Formatter() {
+            @Override
+            public String format(String str) {
+                if (str != null) {
+                    return "[" + str + "]";
+                }
+                return "no data";
+            }
+        });
+        map.put(ImageAdapter.EXIF_MODEL, new ImageAdapter.Formatter() {
+            @Override
+            public String format(String str) {
+                if (str != null) {
+                    return "[" + str + "]";
+                }
+                return "no data";
+            }
+        });
+        map.put(ImageAdapter.EXIF_DATETIME_ORIGINAL, new ImageAdapter.Formatter() {
+            @Override
+            public String format(String str) {
+                if (str != null) {
+                    String[] parts = str.split("\\s+");
+                    if (parts.length > 0) {
+                        return "[" + parts[0].replace(':', '/') + "]";
+                    }
+                }
+                return "no data";
+            }
+        });
+        return map;
+    }
+
+    //
+    // Runtime Permission
+    //
+
+    private static final int PERMISSIONS_REQUEST_ID = 100;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean checkPermission() {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_ID);
+            return false;
+        }
+        return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        if (requestCode == PERMISSIONS_REQUEST_ID &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setImages(mFragment);
+        }
+    }
+
+}
